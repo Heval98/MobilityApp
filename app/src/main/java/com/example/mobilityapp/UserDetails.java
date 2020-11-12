db876f4425a1d14132f3c1f3aefa7308bb0ca090
Juan Escobar <juand-escobart@unilibre.edu.co>
2020-11-12 17:40:09 -0500

    chat

---
+++ app/src/main/java/com/example/mobilityapp/UserDetails.java
@@ -0,0 +1,13 @@
+package com.example.mobilityapp;
+
+public class UserDetails {
+
+    // Variables de usuario
+
+    static String username = "";
+
+    static String password = "";
+
+    static String chatWith = "";
+}
+
